55dabbf923ec19cb7452d5e08905212e399951fd
DESKTOP-S6DAD6J\A8-PC100 <asd@svalero.com>
2023-03-07 20:47:15 +0100

    ONGOING Home main

---
+++ src/components/home/Main.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-main class="">
+    <div class="mt-8">
+      <h1 class="text-5xl">We Serve, You enjoy</h1>
+    </div>
+  </v-main>
+</template>
+
+<script>
+export default {};
+</script>
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Mukta:wght@600&display=swap");
+h1 {
+  font-family: "Mukta", sans-serif;
+}
+</style>
