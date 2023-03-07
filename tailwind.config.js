e5cc853e5aa5ad96946c7ecde7eafcea97e83755
DESKTOP-S6DAD6J\A8-PC100 <asd@svalero.com>
2023-03-07 20:11:29 +0100

    Add basic header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: { "awesome-color": "#7DD3FC" },
+    },
   },
   plugins: [],
 }
