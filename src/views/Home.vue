024d3d1de2ee3b70583d6566979a720eae553f80
SamiFraca <sami.fraca@flat101.es>
2023-03-08 18:46:49 +0100

    Add sample request api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,26 @@
 <template>
- <Header />
- 
+  <Header />
+  <Main />
 </template>
 
 <script>
 // @ is an alias to /src
-// import Header from '@/components/Header.vue'
+import { Header } from "@/components/header/index.js";
+import Main from "@/components/home/Main.vue";
+import axios from 'axios'
 
 export default {
-  name: 'Header',
-  components: {
-    
+  name: "Home",
+  components: { Header, Main },
+  methods: {
+    getList() {
+      axios.get('https://api.sampleapis.com/coffee/hot').then((response) => {
+        console.log(response.data);
+      });
+    },
+  },
+  beforeMount(){
+    this.getList();
   }
-}
+};
 </script>
