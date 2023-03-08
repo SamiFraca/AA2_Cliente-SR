59d7076d0edab4f67b05dfa368a85471c43a09aa
SamiFraca <sami.fraca@flat101.es>
2023-03-08 20:45:36 +0100

    Add sign in view template

---
+++ src/views/Sign.vue
@@ -0,0 +1,13 @@
+<template>
+  <Header />
+</template>
+
+<script>
+import Header from "@/components/header/Header.vue";
+// @ is an alias to /src
+export default {
+  name: "Sign",
+  components: { Header },
+  methods: {},
+};
+</script>
