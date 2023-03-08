f8118a600bf7a3c18f4d7c915f98b5714283ba26
SamiFraca <sami.fraca@flat101.es>
2023-03-08 18:00:56 +0100

    Comment annoying warning for now

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,12 +5,12 @@
 
 <script>
 // @ is an alias to /src
-import Header from '@/components/Header.vue'
+// import Header from '@/components/Header.vue'
 
 export default {
   name: 'Header',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
