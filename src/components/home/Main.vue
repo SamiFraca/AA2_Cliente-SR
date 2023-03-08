5aca7fa594415f5aff8605048df0fe762177b79d
SamiFraca <sami.fraca@flat101.es>
2023-03-08 13:33:57 +0100

    Add basic responsive for filters

--- src/components/home/Main.vue
+++ src/components/home/Main.vue
@@ -1,8 +1,18 @@
 <template>
-  <v-main class="">
-    <div class="mt-8">
+  <v-main>
+    <div class="mt-12">
       <h1 class="text-5xl">We Serve, You enjoy</h1>
     </div>
+    <div class="sm:flex sm:m-auto mt-8">
+      <div class="m-auto flex flex-col">
+        <input
+          type="text"
+          class="px-8 py-3 border-2 sm:rounded-lg sm:mr-8 sm:mt-8 sm:ml-8" placeholder="Name"
+        /><input type="text" class="px-8 py-3 border-2 rounded-lg sm:mr-8 mt-8 sm:mt-0" placeholder="Location"/>
+        <input type="text" class="px-8 py-3 border-2 rounded-lg mt-8 sm:w-full md:mt-8" placeholder="Sport" />
+      </div>
+    </div>
+    <div><img src="../../assets/GQ_50Greatest_final_v2.webp" class="hidden sm:block"></div>
   </v-main>
 </template>
 
@@ -13,5 +23,8 @@ export default {};
 @import url("https://fonts.googleapis.com/css2?family=Mukta:wght@600&display=swap");
 h1 {
   font-family: "Mukta", sans-serif;
+}
+body{
+  overflow: hidden;
 }
 </style>
