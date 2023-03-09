9f6b218be50f3c2c582a7ee9e6369742632e31c2
SamiFraca <sami.fraca@flat101.es>
2023-03-09 18:35:08 +0100

    Add css classes to inputs and form

--- src/components/sign/Inputs.vue
+++ src/components/sign/Inputs.vue
@@ -1,7 +1,46 @@
 <template>
-  <div class="bg-white w-sign-width h-sign-height relative rounded-lg">
+  <div
+    class="bg-white w-sign-width h-sign-height relative rounded-lg flex justify-center"
+  >
     <form @submit.prevent="Data">
-      <input v-model="inputData" type="text" placeholder="Enter data" />
+      <div class="flex flex-col my-12 justify-center">
+        <input
+          v-model="inputData"
+          type="text"
+          placeholder="Username"
+          class="border-solid h-16 w-96 px-4 border-gray-300 border-solid border-2 px-4 rounded-lg"
+        />
+        <input
+          v-model="inputData"
+          type="text"
+          placeholder="Username"
+          class="border-solid h-16 w-96 px-4 border-gray-300 border-solid border-2 px-4 rounded-lg mt-8"
+        />
+        <input
+          v-model="inputData"
+          type="text"
+          placeholder="Username"
+          class="border-solid h-16 w-96 px-4 border-gray-300 border-solid border-2 px-4 rounded-lg mt-8"
+        />
+        <div class="mt-8 flex flex-col">
+          <h3>Are you a bar owner?</h3>
+          <div class="flex mt-8">
+            <button
+              class="bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 py-2 rounded"
+            >
+              Yes
+            </button>
+            <button
+              class="bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 py-2 rounded"
+            >
+              No
+            </button>
+          </div>
+        </div>
+      </div>
+      <button
+        class="bg-blue-700 hover:bg-blue-900  text-white font-bold px-4 py-2 rounded"
+      >Register</button>
     </form>
   </div>
 </template>
