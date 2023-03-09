466a541f44ed3d7cc66fcdf0e248e2ca945c192c
SamiFraca <sami.fraca@flat101.es>
2023-03-09 18:13:35 +0100

    Add script for mac info

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,20 +7,23 @@
 // @ is an alias to /src
 import { Header } from "@/components/header/index.js";
 import Main from "@/components/home/Main.vue";
-import axios from 'axios'
+import axios from "axios";
 
 export default {
   name: "Home",
   components: { Header, Main },
   methods: {
     getList() {
-      axios.get('https://api.sampleapis.com/coffee/hot').then((response) => {
+      axios.get("https://api.sampleapis.com/coffee/hot").then((response) => {
         console.log(response.data);
       });
     },
   },
-  beforeMount(){
+  beforeMount() {
     this.getList();
-  }
+  },
 };
 </script>
+<style>
+
+</style>
