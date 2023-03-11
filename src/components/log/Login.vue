f20a395515eb4ba810326aa35db0d56664e5eef2
Sami Fraca <fracasami@gmail.com>
2023-03-11 21:46:53 +0100

    Modify password and username to required

--- src/components/log/Login.vue
+++ src/components/log/Login.vue
@@ -2,9 +2,9 @@
     <div class="bg-white w-login-width h-login-height relative rounded-lg flex justify-center items-center">
         <form @submit.prevent="submitForm">
             <div class="flex flex-col my-12 justify-center">
-                <input v-model="Username" type="text" placeholder="Username"
+                <input v-model="Username" type="text" placeholder="Username" required
                     class="border-solid h-16 w-96 px-4 border-gray-300 border-solid border-2 px-4 rounded-lg" />
-                <input v-model="Password" type="password" placeholder="Password"
+                <input v-model="Password" type="password" placeholder="Password" required
                     class="border-solid h-16 w-96 px-4 border-gray-300 border-solid border-2 px-4 rounded-lg mt-8" />
                 <br>
             </div>
