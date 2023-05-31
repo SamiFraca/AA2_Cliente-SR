33f034d8094be0d75097b3cfb1b34ca10cdcd4d9
SamiFraca <sami.fraca@flat101.es>
2023-05-31 20:38:13 +0200

    Create loader component

---
+++ src/components/loader/Loader.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="loader">
+      <div class="loader-spinner"></div>
+    </div>
+  </template>
+  
+  <style>
+  .loader {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+  }
+  
+  .loader-spinner {
+    border: 4px solid #f3f3f3; /* Light gray */
+    border-top: 4px solid #3498db; /* Blue */
+    border-radius: 50%;
+    width: 30px;
+    height: 30px;
+    animation: spin 1s linear infinite;
+  }
+  
+  @keyframes spin {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+  }
+  </style>
+  
