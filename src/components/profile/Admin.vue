b8550f4810ae392bfe38e5138de1de9f9d97a1a8
SamiFraca <sami.fraca@flat101.es>
2023-06-08 16:12:46 +0200

    Fix margin display on admin

--- src/components/profile/Admin.vue
+++ src/components/profile/Admin.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col md:flex-row mx-auto">
+  <div class="flex flex-col md:flex-row">
     <SideMenu activeLink="admin" />
     <div
       class="flex flex-col p-4 max-w-5xl text-left justify-start w-full mx-auto mt-4"
