25b4f5cf55a39748c65d4c574c90d859730ca8f8
SamiFraca <sami.fraca@flat101.es>
2023-06-10 21:30:07 +0200

    Add new translations

--- src/main.js
+++ src/main.js
@@ -3,57 +3,68 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./index.css";
-import { createI18n } from 'vue-i18n'
+import { createI18n } from "vue-i18n";
 
 const app = createApp(App);
 const i18n = createI18n({
-  locale: 'en', 
+  locale: "en",
   messages: {
     es: {
       message: {
-        welcome: 'Bienvenido de vuelta',
-        slogan:'Nosotros servimos, tu te entretienes',
-        name:"Nombre",
-        location:"Localización",
-        sport:"Deporte",
-        logIn:"Iniciar Sesión",
-        signIn:"Registrar",
-        errorPassword:"La contraseña no coincide. Porfavor, intentelo de nuevo.",
-        barOwner:"Eres dueño de un bar?",
-        yes:"Si",
-        password:"Contraseña",
-        invalid:"Usuario o contraseña invalido. Porfavor, intentelo de nuevo.",
-        events:"Eventos",
-        capacity:"Capacidad",
-        closeSession:"Cerrar sesión",
-        account:"Cuenta",
-        hello:"Hola"
-      }
+        welcome: "Bienvenido de vuelta",
+        slogan: "Nosotros servimos, tu te entretienes",
+        name: "Nombre",
+        location: "Localización",
+        sport: "Deporte",
+        logIn: "Iniciar Sesión",
+        signIn: "Registrar",
+        errorPassword:
+          "La contraseña no coincide. Porfavor, intentelo de nuevo.",
+        barOwner: "Eres dueño de un bar?",
+        yes: "Si",
+        password: "Contraseña",
+        invalid: "Usuario o contraseña invalido. Porfavor, intentelo de nuevo.",
+        events: "Eventos",
+        capacity: "Capacidad",
+        closeSession: "Cerrar sesión",
+        account: "Cuenta",
+        hello: "Hola",
+        shows: "Eventos",
+        description: "Descripción",
+        country: "País",
+        city: "Ciudad",
+        address: "Dirección",
+      },
     },
     en: {
       message: {
-        welcome: 'Welcome back',
-        slogan:'We serve, you enjoy',
-        name:"Name",
-        location:"Location",
-        sport:"Sport",
-        logIn:"Log in",
-        signIn:"Sign in",
-        errorPassword:"Password doesn't match. Please, try again.",
-        barOwner:"Are you a bar owner?",
-        yes:"Yes",
-        no:"No",
-        password:"Password",
-        invalid:"Invalid Username or password. Please try again.",
-        events:"Events",
-        capacity:"Capacidad",
-        closeSession:"Logout",
-        account:"Account",
-        hello:"Hi"
-      }
-    }
-  }
-})
+        welcome: "Welcome back",
+        slogan: "We serve, you enjoy",
+        name: "Name",
+        location: "Location",
+        sport: "Sport",
+        logIn: "Log in",
+        signIn: "Sign in",
+        errorPassword: "Password doesn't match. Please, try again.",
+        barOwner: "Are you a bar owner?",
+        yes: "Yes",
+        no: "No",
+        password: "Password",
+        invalid: "Invalid Username or password. Please try again.",
+        events: "Events",
+        capacity: "Capacidad",
+        closeSession: "Logout",
+        account: "Account",
+        hello: "Hi",
+        shows: "Events",
+        description: "Description",
+        country: "Country",
+        city: "City",
+        address: "Address",
+      },
+    },
+  },
+});
 app.use(router);
 app.use(store);
 app.use(i18n);
